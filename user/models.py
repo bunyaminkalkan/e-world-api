93d820a6307551701471be5e74f58ea9ea2dcbf3
Bunyamin Kalkan <bnkalkan41@gmail.com>
2023-11-12 14:04:49 +0300

    update serializer and usermodel

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from card.models import Card
 
 class UserModel(User):
-    cards = models.ManyToManyField(Card)
+    cards = models.ManyToManyField(Card, blank=True)
     balance = models.IntegerField(default=5000)
 
     def __str__(self):
--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import UserModel
-from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,4 +11,21 @@ class UserSerializer(serializers.ModelSerializer):
             "date_joined",
             "groups",
             "user_permissions",
+            "cards",
+            "is_staff",
+            "is_active",
+            "is_superuser",
         ]
+
+    def validate(self, attrs):
+        if attrs.get('password', False):
+            from django.contrib.auth.password_validation import validate_password #doğrulama
+            from django.contrib.auth.hashers import make_password #hashleme
+            password = attrs['password'] #şifreyi al
+            validate_password(password) #doğrulamadan geçir
+            attrs.update(
+                {
+                    'password': make_password(password) # password şifrele ve güncelle
+                }
+            )
+        return super().validate(attrs) #Orjinal metodu çalıştır
