0ba0315ceed98520645c9c317b258907b34ae02a
Bunyamin Kalkan <bnkalkan41@gmail.com>
2023-12-14 20:54:21 +0300

    change card list and cors headers

--- card/views.py
+++ card/views.py
@@ -10,7 +10,7 @@ from .pagination import CustomPageNumberPagination
 class CardListPurchaseAPIView(ListCreateAPIView):
     '''
     Card list and for users to purchase cards view
-    If method is GET all cards are listed
+    If method is GET all cards are listed and to be used in the frontend, if the user is logged in and has cards, show the price of the cards the user has as 0.
     If method is POST the logged user can purchase a card if his/her balance is sufficient.
     '''
 
@@ -18,6 +18,24 @@ class CardListPurchaseAPIView(ListCreateAPIView):
     serializer_class = CardSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,) # Permission class that anyone can view but only logged in users can make purchases
     pagination_class = CustomPageNumberPagination
+        
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            if request.user.is_authenticated: # if user is logged in
+                user = UserModel.objects.get(id=request.user.id) # get user
+                for i in range(len(user.cards.values_list())): # number of cards the user has
+                    card_price = user.cards.values_list()[i][4] # get price for the cards the user has
+                    for j in range(len(serializer.data)): # browse all cards
+                        if card_price == serializer.data[j]['price']: # find cards owned by user
+                            serializer.data[j]['price'] = 0 # set card price equal to 0
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def post(self, request):
         # Add record manytomany table for purchase
@@ -25,8 +43,11 @@ class CardListPurchaseAPIView(ListCreateAPIView):
         card = Card.objects.get(cardname=request.data['cardname'])
         if user.balance >= card.price:
             new_balance = user.balance - card.price
+            new_price = card.price * 1.22
             user.balance = new_balance
+            card.price = int(new_price)
             user.save()
+            card.save()
             user.cards.add(card)
             data = {'balance': new_balance, 'purchase': 'Successfully'}
             return Response(data, status=status.HTTP_201_CREATED)
--- main/settings.py
+++ main/settings.py
@@ -52,14 +52,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    #CorsHeaders
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'main.urls'
